e80e1a3b6326fefdd30c38772bd37f771debd5d7
CannonBrave <142853122+CannonBrave@users.noreply.github.com>
2023-09-13 23:53:01 +0000

    if elif example

--- src/examples/c_decisions/decisions.py
+++ src/examples/c_decisions/decisions.py
@@ -32,3 +32,22 @@ def is_overtime(hours):
     result = False
     result = hours > 40
     return result
+
+def get_letter_grade(grade):
+    letter_grade = ""
+
+    if(grade >= 90 and grade <= 100):
+        letter_grade = "A"
+    elif(grade >= 80 and grade < 90):
+        letter_grade = "B"
+    elif(grade >= 70 and grade < 80):
+        letter_grade = "C"
+    elif(grade >= 60 and grade < 70):
+        letter_grade = "D"
+    elif(grade >= 0 and grade < 60):
+        letter_grade = "F"
+    else:
+        letter_grade = "Invalid Grade"
+
+    return letter_grade
+        
--- tests/examples/c_decisions/tests_decisions.py
+++ tests/examples/c_decisions/tests_decisions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.c_decisions.decisions import get_and_result, get_notted_value, get_or_result, is_consonant, is_even, is_odd, is_overtime, is_vowel, test_config
+from src.examples.c_decisions.decisions import get_and_result, get_letter_grade, get_notted_value, get_or_result, is_consonant, is_even, is_odd, is_overtime, is_vowel, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -48,3 +48,14 @@ class Test_Config(unittest.TestCase):
     def test_is_overtime(self):
         self.assertEqual(False, is_overtime(30))
         self.assertEqual(True, is_overtime(41))
+
+    def test_get_letter_grade(self):
+        self.assertEqual(get_letter_grade(105), "Invalid Grade")
+        self.assertEqual(get_letter_grade(100), "A")
+        self.assertEqual(get_letter_grade(95), "A")
+        self.assertEqual(get_letter_grade(90), "A")
+        self.assertEqual(get_letter_grade(85), "B")
+        self.assertEqual(get_letter_grade(75), "C")
+        self.assertEqual(get_letter_grade(65), "D")
+        self.assertEqual(get_letter_grade(55), "F")
+        self.assertEqual(get_letter_grade(-5), "Invalid Grade")
